0439880fa9fed49a8213972469e87ad2b7511a56
Ajinkya-06 <ajinkyac481@gmail.com>
2025-03-04 18:46:03 +0530

    pickle file generated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ from src.exception import CustmeException
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path =os.path.join("artifacts/data_ingestion", "train.csv")
@@ -46,3 +47,7 @@ if __name__ == "__main__":
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
     #src\components\data_ingestion.py
+    
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,9 @@ from src.logger import logging
 from src.exception import CustmeException
 import pickle
 import os, sys
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_recall_curve, f1_score, precision_score, recall_score
+from sklearn.model_selection import GridSearchCV
+
 
 def save_object(file_path, obj):
     try:
@@ -13,3 +16,38 @@ def save_object(file_path, obj):
             pickle.dump(obj, file_obj)
     except Exception as e:
         raise CustmeException(sys, e)
+    
+
+def evaluate_model(X_train, X_test, y_train, y_test, models, params):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model_name = list(models.keys())[i]  # Get model name
+            model = models[model_name]  # Get model object
+            para = params[model_name]
+            
+            GS = GridSearchCV(model, para, cv = 5)
+            GS.fit(X_train,y_train)
+            
+            model.set_params(**GS.best_params_)
+            model.fit(X_train, y_train)
+            
+            y_pred = model.predict(X_test)
+            test_model_accuracy = accuracy_score(y_test, y_pred)
+            
+            report[list(models.values())[i]] = test_model_accuracy
+            
+            return report
+            
+    
+    except Exception as e:
+        raise CustmeException(e, sys)
+        
+        
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_objt:
+            return pickle.load(file_objt)
+    except Exception as e:
+        raise CustmeException(e, sys)
+
